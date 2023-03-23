5704026221aef5d141535c50cafeef3e433f7f8a
alxqiu <alxqiu@gmail.com>
2023-03-23 10:25:25 -0400

    tweaked navbar to exist after navigating to new page, although not sticky

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import Navigation from "./navigation";
 import Login from "./login";
 import Search from "./search";
+import Home from "./home";
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Nav} from "react-bootstrap";
 
 function App() {
   return (
-      <div>
+      <div className="container-fluid">
           <BrowserRouter>
+              <Navigation/>
               <Routes>
-                  <Route index element={<Navigation/>}/>
+                  <Route index element={<Home/>}/>
                   <Route path="/login" element={<Login/>}/>
                   <Route path="/search/:query" element={<Search/>}/>
               </Routes>
           </BrowserRouter>
-          <div className="App mt-2">
-              {/* <Navigation></Navigation>*/}
-              <Login></Login>
-          </div>
       </div>
   );
 }
