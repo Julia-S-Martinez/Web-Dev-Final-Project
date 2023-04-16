0d0db6e77bba75320cb5b6018811e8d2fd366a89
Emily Gringorten <emily.gringorten@gmail.com>
2023-04-16 14:05:22 -0400

    small styling tweaks to navbar and posts

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -23,13 +23,17 @@ const Navigation = (
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark m-2 ">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Home</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02"
                         aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarColor02">
-                    <ul className="navbar-nav me-auto">
+                    <form className="d-flex">
+                        <input className="form-control me-sm-2 w-full" type="search" placeholder="Search" onChange={editSearch}/>
+                        <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={submitSearch}>Search</button>
+                    </form>
+                    <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
                             <a className="nav-link active" href="/">Home
                                 <span className="visually-hidden">(current)</span>
@@ -41,25 +45,10 @@ const Navigation = (
                         <li className="nav-item">
                             <a className="nav-link" href="#">Pricing</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div className="dropdown-menu">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Separated link</a>
-                            </div>
+                        <li className="nav-item ml-auto">
+                            <a className="nav-link" href="/my-profile">Profile</a>
                         </li>
                     </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-sm-2" type="search" placeholder="Search" onChange={editSearch}/>
-                            <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={submitSearch}>Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
